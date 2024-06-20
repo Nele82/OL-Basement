9c9b4beeef383f24664a0d5943c71f6890b65b70
Nebojsa <nebojsap82@gmail.com>
2024-06-20 17:00:03 +0200

    Adding a "localhost:3000" address for development and presentation purposes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,6 +48,7 @@ const requestReset = async (req, res) => {
     }
   
     const token = jwtoken.sign({ id: user._id }, process.env.SECRET_KEY, { expiresIn: '600000' })
+    // const resetLink = `http://localhost:3000/password-reset?token=${token}`
     const resetLink = `https://ol-basement.netlify.app/password-reset?token=${token}`
   
     const mailjet = Mailjet.apiConnect(
